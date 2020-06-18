9186517fade066678c20f0e45e140b3a365184ae
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-18 22:25:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,6 +4,11 @@ class Application
     resp = Rack::Response.new # Create a new Response
     
     # Get the current time
+    if Time.now.hour < 12
+      resp.write("Good Morning!")
+    else
+      resp.write("Good Afternoon!")
+    end
     
     resp.finish
   end
